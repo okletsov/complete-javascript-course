e1cceb6ec4316bcee3cec9332606722d3109b512
Oleksii Kletsov <akletsov88@gmail.com>
2019-01-12 20:42:53 -0500

    wip

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -10,16 +10,7 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0; //Player 1 by default
-
- document.querySelector('.dice').style.display = 'none'; // changing css style
-
- document.querySelector('#score-0').textContent = 0;
- document.querySelector('#score-1').textContent = 0;
- document.getElementById('current-0').textContent = 0;
- document.getElementById('current-1').textContent = 0;
+init();
 
 // anonymous functoin as parameter
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -37,19 +28,54 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice; // same as roundScore = roundScore + dice
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        // next player
         document.getElementById('current-' + activePlayer).textContent = '0'; // set player's round score to 0
         
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // change active player
-        roundScore = 0; 
-
-        document.querySelector('.player-0-panel').classList.toggle('active');  // toggle adds class 'active if it is absent and adds if it is present'
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        
-        diceDOM.style.display = 'none';
+        // next player
+        nextPlayer();
     }
-
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add current score to global score
+    scores[activePlayer] += roundScore;
+    document.getElementById('current-' + activePlayer).textContent = 0;
+
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if(scores[activePlayer] >= 100) {
+        document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('.winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // change active player
+    roundScore = 0; 
+
+    document.querySelector('.player-0-panel').classList.toggle('active');  // toggle adds class 'active if it is absent and adds if it is present'
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+} 
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'none'; // changing css style
+    document.querySelector('#score-0').textContent = 0;
+    document.querySelector('#score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+}
 
 
 // Accessing element in DOM and assigning a text value...
