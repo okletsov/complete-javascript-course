5b9333a6b76947a2439cbe5635301261dce2161a
Oleksii Kletsov <okletsov@cainc.com>
2019-01-18 16:39:23 -0500

    Closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -157,7 +157,7 @@ interviewQuestion('teacher')('Jane');
 */
 
 // IIFE
-
+/*
 function game() {
     var score = Math.random() * 10;
     console.log(score >= 5);
@@ -174,3 +174,54 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
 }) (2);
+*/
+
+// Closures
+
+function retirement(retirementAge) {
+    var a = ' years until retirement.';
+    return function(yearOfBirth) {
+        var age = 2019 - yearOfBirth;
+        console.log((retirementAge - age) +a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+/*
+function interviewQuestion(job) {
+    if(job === 'designer') {
+        return function(name) {
+            console.log(name + ' can you explain what UX design is?');
+        }
+    } else if(job === 'teacher') {
+        return function(name) {
+            console.log('What subjet do you teach, ' + name + '?');
+        }
+    } return function(name) {
+        console.log('Hello, ' + name + ', what do you do?');
+    }
+}
+*/
+
+function interviewQuestion(job) {
+    return function(name) {
+        if(job === 'designer') {
+            console.log(name + ', can you explain what UX design is?');
+        } else if(job === 'teacher') {
+            console.log('What subjet do you teach, ' + name + '?');
+        } else {
+            console.log('Hello, ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
+interviewQuestion('designer')('Mark');
+interviewQuestion('driver')('Jane');
