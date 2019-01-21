d712b8fd03958d1a3a880b01d8a9a032ecc82b00
Oleksii Kletsov <akletsov88@gmail.com>
2019-01-21 11:29:11 -0500

    keypress event listener

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,30 +1,42 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-           return add(b);
-        }
-    };
-
-}) ();
-
-var uiController = (function() {
+    
     // Some code
+
 }) ();
 
+// UI CONTROLLER
+var uiController = (function() {
+    
+    // Some code
+
+}) ();
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, uiCtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
+        // 1. Get the input field data
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
+        // 2. Add the item to the budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget
+
+        console.log('It works');
+    
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) { // event parameter automatically gets passed to function by browser
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
         }
-    }
+    });
 
 }) (budgetController, uiController);
