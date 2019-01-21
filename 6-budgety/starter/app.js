5118cf25c818236f895887588177111a9fa09814
Oleksii Kletsov <akletsov88@gmail.com>
2019-01-21 12:05:12 -0500

    reading input data, creating public methods

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -8,16 +8,39 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var uiController = (function() {
+
+    var domStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     
-    // Some code
+    return {
+        getInput: function() {
+            return {
+            type: document.querySelector(domStrings.inputType).value, // Will be either "inc" or "exp"
+            description: document.querySelector(domStrings.inputDescription).value,
+            value: document.querySelector(domStrings.inputValue).value
+            };
+        },
+
+        getDomStrings: function() {
+            return domStrings;
+        }
+    };
 
 }) ();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, uiCtrl) {
 
+    var dom = uiCtrl.getDomStrings();
+
     var ctrlAddItem = function() {
         // 1. Get the input field data
+        var input = uiCtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -26,12 +49,10 @@ var controller = (function(budgetCtrl, uiCtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget
-
-        console.log('It works');
     
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(dom.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) { // event parameter automatically gets passed to function by browser
         if(event.keyCode === 13 || event.which === 13) {
