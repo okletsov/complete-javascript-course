f7da20b50dc7ae53b75cdeeb9a4b10ad26086633
Oleksii Kletsov <okletsov@cainc.com>
2019-01-24 21:13:28 -0500

    adding new item to data structure

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -26,6 +26,37 @@ var budgetController = (function() {
         }
     };
 
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+
+            // Create new item based on 'inc' or 'exp' type
+            if(type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if(type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into data structure
+            data.allItems[type].push(newItem);
+
+            // Return the element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    }
+
 }) ();
 
 // UI CONTROLLER
@@ -70,10 +101,13 @@ var controller = (function(budgetCtrl, uiCtrl) {
     };
 
     var ctrlAddItem = function() {
+        var input, newItem;
+        
         // 1. Get the input field data
-        var input = uiCtrl.getInput();
+        input = uiCtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add item to the UI
 
