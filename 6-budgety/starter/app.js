c9a3d8f71a10de2d904d3c59537ed21a0a863e2b
Oleksii Kletsov <akletsov88@gmail.com>
2019-02-03 22:33:05 -0500

    formatting numbers part 1

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -237,6 +237,23 @@ var uiController = (function() {
             });
         },
 
+        formatNumber: function(num, type, sign) {
+            var numSplit, int, dec; 
+            
+            num = Math.abs(num);
+            num = num.toFixed(2);
+
+            numSplit = num.split('.');
+            int = numSplit[0];
+            if(int.length > 3) {
+                int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+            }
+
+            dec = numSplit[1];
+
+            return (type === 'exp' ? '-' : '+') + ' ' + int + dec;
+        },
+
         getDomStrings: function() {
             return domStrings;
         }
