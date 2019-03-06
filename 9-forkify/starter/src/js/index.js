6597001889ce782f0fe4c61b790a362f0c56bc29
Oleksii Kletsov <akletsov88@gmail.com>
2019-03-05 21:34:25 -0500

    how export and import from modules work

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,5 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import str from './models/Search';
+// import {add as a, multiply as m, ID} from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(`Using imported functoins! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}.`);
