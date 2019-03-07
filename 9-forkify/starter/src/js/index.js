acaa00e08d60df367a96e05c856038116dcd4cea
Oleksii Kletsov <akletsov88@gmail.com>
2019-03-06 21:55:56 -0500

    added the Search class

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = 'e12418e38326498b9c872cfceccfd6ed';
-    
-    try {
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        alert(error);
-    }
-    
-}
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
---
+++ 9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'e12418e38326498b9c872cfceccfd6ed';
+        
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch(error) {
+            alert(error);
+        }
+    }
+}
