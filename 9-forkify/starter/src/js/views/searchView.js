a3e3c149a944d77705ef15fefa71e24a03e06ece
Oleksii Kletsov <akletsov88@gmail.com>
2019-03-10 23:49:10 -0400

    implemented truncation of recipe title, using reduce method on an array

--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -10,6 +10,22 @@ export const clearResults = () => {
     elements.searchResultList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <=17) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length; // return inside a reduce function updates accumulator
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')} ...`; // join is to separate all words of an array with a space
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -18,7 +34,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
