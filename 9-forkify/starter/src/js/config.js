8e0864c32b8aa73729b0e6c29addd60d03e2e7d0
Oleksii Kletsov <akletsov88@gmail.com>
2019-03-18 23:13:13 -0400

    added a recipe model

---
+++ 9-forkify/starter/src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'e12418e38326498b9c872cfceccfd6ed';
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {key, proxy} from '../config';
 
 export default class Search {
     constructor(query) {
@@ -6,9 +7,7 @@ export default class Search {
     }
 
     async getResults() {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = 'e12418e38326498b9c872cfceccfd6ed';
-        
+                
         try {
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
