9d11f09709aafc5764b8ebc87018fe2bf422982b
Oleksii Kletsov <akletsov88@gmail.com>
2019-03-19 22:58:16 -0400

    building the recipe controller, adding event handler to multiple events

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -27,12 +27,17 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4) Search for recipes
-        await state.search.getResults();
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
 
-        // 5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);   
+        } catch (error) {
+            alert('Something wrong with the search');
+            clearLoader();
+        }
     }
 }
 
@@ -54,7 +59,32 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * RECIPE CONTROLLER
  */
+const controlRecipe = async () => {
+    // Get id from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
 
- const r = new Recipe(35626);
- r.getRecipe();
- console.log(r);
+    if(id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe();
+
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            console.log(state.recipe);    
+        } catch (error) {
+            alert('Error processing recipe');
+        }
+    }
+};
+
+ // Adding event listener to multiple events
+ ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
